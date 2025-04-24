5211dbbf003dcb30dd47dcdaca1355e8eb7eca3c
Maria Vitória Martelli <martellimv@gmail.com>
2025-04-24 18:58:32 -0300

    alteracao mais recente

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 public class Conexao {
     public static Connection conectar() throws SQLException {
             return DriverManager.getConnection(
-        	"jdbc:postgresql://localhost:5432/projetoFinalPOO", // nome do banco
-        	"postgres",                                 // usuário padrão
-        	"root"                            // senha que você usa no pgAdmin
+        	"jdbc:postgresql://localhost:5432/projetoFinalPOO", 
+        	"postgres",                                 
+        	"root"                            
         	);
 
     }
