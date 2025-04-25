c696f77c468f180cdd5d7c47f4b7b1e8a6a853a2
JUOMATSU <julianaomatsup@gmail.com>
2025-04-25 17:18:10 -0300

    pasta funcionario

--- src/exception/CpfDuplicadoException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class CpfDuplicadoException extends Exception {
-    public CpfDuplicadoException(String mensagem) {
-        super(mensagem);
-    }
-}
--- src/exception/DependenteException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class DependenteException extends Exception {
-    public DependenteException(String mensagem) {
-        super(mensagem);
-    }
-}
